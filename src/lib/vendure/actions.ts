aff8a16dabe6dda11c52b5783f1b2f1525825db2
ssirec <125913769+ssirec@users.noreply.github.com>
2026-04-10 17:50:26 +0200

    Update actions.ts

--- src/lib/vendure/actions.ts
+++ src/lib/vendure/actions.ts
@@ -1,18 +1,26 @@
-import {query} from './api';
-import {GetActiveCustomerQuery} from './queries';
-import {getActiveChannelCached} from './cached';
-import {cache} from "react";
-import {readFragment} from "@/graphql";
-import {ActiveCustomerFragment} from "@/lib/vendure/fragments";
-import {getAuthToken} from "@/lib/auth";
-
+import { query } from './api';
+import { GetActiveCustomerQuery } from './queries';
+import { getActiveChannelCached } from './cached';
+import { cache } from "react";
+import { readFragment } from "@/graphql";
+import { ActiveCustomerFragment } from "@/lib/vendure/fragments";
+import { getAuthToken } from "@/lib/auth";
 
 export const getActiveCustomer = cache(async () => {
     const token = await getAuthToken();
-    const result = await query(GetActiveCustomerQuery, undefined, {
-        token
-    });
+
+    const result = await query(
+        GetActiveCustomerQuery,
+        undefined,
+        { token }
+    );
+
+    // If no active customer, return null safely
+    if (!result.data.activeCustomer) {
+        return null;
+    }
+
     return readFragment(ActiveCustomerFragment, result.data.activeCustomer);
-})
+});
 
 export const getActiveChannel = getActiveChannelCached;
