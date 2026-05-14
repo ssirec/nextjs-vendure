40077594db981a90b0e7213bd738be655f7a3fd0
ssirec <125913769+ssirec@users.noreply.github.com>
2026-05-14 13:38:40 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  cacheComponents: true,
   serverExternalPackages: [
     "@swc/core",
     "@swc/core-linux-x64-gnu",
     "@swc/core-linux-x64-musl",
     "@swc/wasm",
   ],
-  experimental: {
-    cacheComponents: true,
-  },
 };
 
 export default nextConfig;
