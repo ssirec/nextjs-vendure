fa040c265843138d765e34b792da41919f02f308
ssirec <125913769+ssirec@users.noreply.github.com>
2026-05-15 11:16:14 +0200

    Simplify CheckoutOrder type definition
    Refactored CheckoutOrder type to simplify structure.

--- src/app/[locale]/checkout/types.ts
+++ src/app/[locale]/checkout/types.ts
@@ -1,16 +1,7 @@
 import { ResultOf } from '@/graphql';
 import { GetActiveOrderForCheckoutQuery } from '@/lib/vendure/queries';
 
-type BaseOrder = NonNullable<ResultOf<typeof GetActiveOrderForCheckoutQuery>['activeOrder']>;
-
-type ShippingLineWithPrice = BaseOrder['shippingLines'][number] & {
-  priceWithTax: number;
-};
-
-export type CheckoutOrder = Omit<BaseOrder, 'shippingLines'> & {
-  shippingLines: Array<ShippingLineWithPrice>;
-};
-
+export type CheckoutOrder = NonNullable<ResultOf<typeof GetActiveOrderForCheckoutQuery>['activeOrder']>;
 export type OrderLine = CheckoutOrder['lines'][number];
 export type ShippingAddress = CheckoutOrder['shippingAddress'];
 export type BillingAddress = CheckoutOrder['billingAddress'];
