f2c134cb769c6b01de981b617bed555f1fd2810c
ssirec <125913769+ssirec@users.noreply.github.com>
2026-05-15 12:25:15 +0200

    Update types.ts

--- src/app/[locale]/checkout/types.ts
+++ src/app/[locale]/checkout/types.ts
@@ -1,16 +1,3 @@
-import { initGraphQLTada } from 'gql.tada';
-import type { introspection } from './graphql-env.d.ts';
-
-export const graphql = initGraphQLTada<{
-  introspection: introspection;
-  scalars: {
-    Money: number;
-    DateTime: string;
-  };
-}>();
-
-export { readFragment } from 'gql.tada';
-export type { ResultOf, VariablesOf } from 'gql.tada';
 import { ResultOf } from '@/graphql';
 import { GetActiveOrderForCheckoutQuery } from '@/lib/vendure/queries';
 
