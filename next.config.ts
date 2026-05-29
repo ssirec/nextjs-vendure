a3332b9987a0af0b13d69d90e77d74b8f587c32f
ssirec <125913769+ssirec@users.noreply.github.com>
2026-05-29 14:19:17 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,6 @@ import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin();
 
 const nextConfig: NextConfig = {
-  cacheComponents: true,
   typescript: {
     ignoreBuildErrors: true,
   },
